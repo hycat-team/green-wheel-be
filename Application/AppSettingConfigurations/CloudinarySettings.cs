47c321cc448e676eea73040c9c2d9ee6235fc713
duck-lawrence <tduc01234@gmail.com>
2025-11-11 20:41:18 +0700

    wip

--- Application/AppSettingConfigurations/CloudinarySettings.cs
+++ Application/AppSettingConfigurations/CloudinarySettings.cs
@@ -2,9 +2,10 @@
 {
     public class CloudinarySettings
     {
-        public string CloudName { get; set; } = ""; 
+        public string CloudName { get; set; } = "";
         public string ApiKey { get; set; } = "";
         public string ApiSecret { get; set; } = "";
         public string ApiFolder { get; set; } = "";
+        public int SignedUrlExpireSeconds { get; set; } = 300;
     }
 }
